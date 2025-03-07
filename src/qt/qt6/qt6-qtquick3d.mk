d59dfcda449269f4585c76f25ab9c34dee85ad6b
mib383 <mib383@gmail.com>
2025-03-07 13:47:06 +0700

    fixed qt6-quick3d.mk and deps

--- src/qt/qt6/qt6-qtquick3d.mk
+++ src/qt/qt6/qt6-qtquick3d.mk
@@ -6,7 +6,10 @@ PKG := qt6-qtquick3d
 $(eval $(QT6_METADATA))
 
 $(PKG)_CHECKSUM := 084cebccb8c5b1c6bafb7756ab89b08ced23c20cd2e996ed54909a154a9f0b6d
-$(PKG)_DEPS     := cc qt6-conf qt6-qtbase qt6-qtdeclarative qt6-qtquicktimeline qt6-qtshadertools
+$(PKG)_TARGETS  := $(BUILD) $(MXE_TARGETS)
+$(PKG)_DEPS_$(BUILD) := qt6-conf qt6-qtbase qt6-qtdeclarative qt6-qtshadertools qt6-qtquicktimeline
+$(PKG)_DEPS          := cc $($(PKG)_DEPS_$(BUILD)) $(BUILD)~$(PKG) assimp
+
 
 QT6_PREFIX   = '$(PREFIX)/$(TARGET)/$(MXE_QT6_ID)'
 QT6_QT_CMAKE = '$(QT6_PREFIX)/$(if $(findstring mingw,$(TARGET)),bin,libexec)/qt-cmake-private' \
@@ -14,6 +17,13 @@ QT6_QT_CMAKE = '$(QT6_PREFIX)/$(if $(findstring mingw,$(TARGET)),bin,libexec)/qt
 
 define $(PKG)_BUILD
     $(QT6_QT_CMAKE) -S '$(SOURCE_DIR)' -B '$(BUILD_DIR)'
+    $(if $(BUILD_STATIC),'$(SED)' -i "/^ *LINK_LIBRARIES = /{s/$$/ `'$(TARGET)-pkg-config' --libs libbrotlidec`/g}" '$(BUILD_DIR)/build.ninja',)
     cd '$(BUILD_DIR)' && '$(TARGET)-cmake' --build . -j '$(JOBS)'
     cd '$(BUILD_DIR)' && '$(TARGET)-cmake' --install .
+endef
+
+define $(PKG)_BUILD_$(BUILD)
+    $(QT6_QT_CMAKE) -S '$(SOURCE_DIR)' -B '$(BUILD_DIR)'
+    cmake --build '$(BUILD_DIR)' -j '$(JOBS)'
+    cmake --install '$(BUILD_DIR)'
 endef
--- src/qt/qt6/qt6-qtquicktimeline.mk
+++ src/qt/qt6/qt6-qtquicktimeline.mk
@@ -6,13 +6,22 @@ PKG := qt6-qtquicktimeline
 $(eval $(QT6_METADATA))
 
 $(PKG)_CHECKSUM := c33dbceed9fabb3d7f6731a68aee602bad5a04bb6f0a473792e61858c0bc1aaa
-$(PKG)_DEPS     := cc qt6-conf qt6-qtbase qt6-qtdeclarative
+$(PKG)_TARGETS  := $(BUILD) $(MXE_TARGETS)
+$(PKG)_DEPS_$(BUILD) := qt6-conf qt6-qtbase qt6-qtdeclarative
+$(PKG)_DEPS          := cc $($(PKG)_DEPS_$(BUILD)) $(BUILD)~$(PKG)
 
 QT6_PREFIX   = '$(PREFIX)/$(TARGET)/$(MXE_QT6_ID)'
 QT6_QT_CMAKE = '$(QT6_PREFIX)/$(if $(findstring mingw,$(TARGET)),bin,libexec)/qt-cmake-private' \
                    -DCMAKE_INSTALL_PREFIX='$(QT6_PREFIX)'
 
 define $(PKG)_BUILD
+    $(QT6_QT_CMAKE) -S '$(SOURCE_DIR)' -B '$(BUILD_DIR)'\
+        -DQT_BUILD_TOOLS_WHEN_CROSSCOMPILING=ON
+    cd '$(BUILD_DIR)' && '$(TARGET)-cmake' --build . -j '$(JOBS)'
+    cd '$(BUILD_DIR)' && '$(TARGET)-cmake' --install .
+endef
+
+define $(PKG)_BUILD_$(BUILD)
     $(QT6_QT_CMAKE) -S '$(SOURCE_DIR)' -B '$(BUILD_DIR)'
     cd '$(BUILD_DIR)' && '$(TARGET)-cmake' --build . -j '$(JOBS)'
     cd '$(BUILD_DIR)' && '$(TARGET)-cmake' --install .

b2b3388fa705cdf93b8d39800ce21d8c42ef1010
mib383 <mib383@gmail.com>
2025-03-23 18:53:15 +0700

    one more fix

--- src/vtk.mk
+++ src/vtk.mk
@@ -23,7 +23,7 @@ $(PKG)_DEPS_$(BUILD) := cmake
 #endef
 #
 define $(PKG)_BUILD_$(BUILD)
-	$(SED) -i "\$a target_link_libraries(SimpleView PRIVATE $(PREFIX)/$(TARGET)/lib/libbrotli.a)" $(SOURCE_DIR)/Examples/GUI/Qt/SimpleView/CMakeLists.txt
+	$(SED) -i '\\$a target_link_libraries(SimpleView PRIVATE $(PREFIX)/$(TARGET)/lib/libbrotli.a)' $(SOURCE_DIR)/Examples/GUI/Qt/SimpleView/CMakeLists.txt
     # first we need a native build to create the compile tools
     # must be built in dest since there's no way to install tools only
     # and the build rules reference certain make targets

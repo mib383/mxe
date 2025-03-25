6a4ea21845473b19bbd94dfa49d1d97c1bdf65b3
mib383 <mib383@gmail.com>
2025-03-26 00:15:19 +0700

    one more fix

--- src/vtk.mk
+++ src/vtk.mk
@@ -44,10 +44,10 @@ define $(PKG)_BUILD
     $(SED) -i 's,d3d9,nod3d9,g' '$(1)/CMake/FindDirectX.cmake'
 	
 	
-	FTLIBS=$($(PREFIX)/$(TARGET)/bin/freetype-config --libs)
-	FTDIR1=$(PREFIX)/$(TARGET)/include/freetype2
-	FTDIR2=$(PREFIX)/$(TARGET)/include/
-	FTDIRS=$(FTDIR1);$(FTDIR1)
+	FTLIBS:=$(shell '$(PREFIX)/$(TARGET)/bin/freetype-config --libs')
+	FTDIR1:=$(PREFIX)/$(TARGET)/include/freetype2
+	FTDIR2:=$(PREFIX)/$(TARGET)/include/
+	FTDIRS:=$(FTDIR1)
 	
 	echo "$(FTLIBS)"
 	echo "$(FTDIR1)"

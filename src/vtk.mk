f7db54c1e0cbdcc7c84039d343d4afd499a8ddd4
mib383 <mib383@gmail.com>
2025-03-25 23:42:55 +0700

    one more fix

--- src/vtk.mk
+++ src/vtk.mk
@@ -44,7 +44,7 @@ define $(PKG)_BUILD
     $(SED) -i 's,d3d9,nod3d9,g' '$(1)/CMake/FindDirectX.cmake'
 	
 	
-	FTLIBS=$($(PREFIX)/$(TARGET)/bin/$(TARGET)-freetype-config --libs)
+	FTLIBS=$($(PREFIX)/$(TARGET)/bin/freetype-config --libs)
 	FTDIR1=$(PREFIX)/$(TARGET)/include/freetype2
 	FTDIR2=$(PREFIX)/$(TARGET)/include/
 	FTDIRS=$(FTDIR1);$(FTDIR1)
@@ -67,7 +67,7 @@ define $(PKG)_BUILD
         -DVTK_FORBID_DOWNLOADS=ON \
         -DBUILD_EXAMPLES=OFF \
         -DBUILD_TESTING=OFF \
-		-DFREETYPE_LIBRARIES="$($(PREFIX)/$(TARGET)/bin/$(TARGET)-freetype-config --libs)" \
+		-DFREETYPE_LIBRARIES="$($(PREFIX)/$(TARGET)/bin/freetype-config --libs)" \
 		-DFREETYPE_INCLUDE_DIR_ft2build="$(PREFIX)/$(TARGET)/include/freetype2" \
 		-DFREETYPE_INCLUDE_DIR_freetype2="$(PREFIX)/$(TARGET)/include" \
 		-DFREETYPE_INCLUDE_DIRS="$(PREFIX)/$(TARGET)/include/freetype2;$(PREFIX)/$(TARGET)/include" \

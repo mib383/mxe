f522e1af2be14127ee2eb4e0d522e330e9f270df
mib383 <mib383@gmail.com>
2025-03-25 22:46:05 +0700

    one more fix

--- src/freetype.mk
+++ src/freetype.mk
@@ -18,7 +18,7 @@ define $(PKG)_UPDATE
 endef
 
 define $(PKG)_BUILD_COMMON
-    cd '$(1)' && GNUMAKE=$(MAKE) ./configure --with-harfbuzz=yes \
+    cd '$(1)' && GNUMAKE=$(MAKE) ./configure --with-harfbuzz=yes  \
         $(MXE_CONFIGURE_OPTS) \
         --enable-freetype-config \
         LIBPNG_CFLAGS="`$(TARGET)-pkg-config libpng --cflags`" \
--- src/vtk.mk
+++ src/vtk.mk
@@ -56,6 +56,10 @@ define $(PKG)_BUILD
         -DVTK_FORBID_DOWNLOADS=ON \
         -DBUILD_EXAMPLES=OFF \
         -DBUILD_TESTING=OFF \
+		-DFREETYPE_LIBRARIES="`$(PREFIX)/$(TARGET)/bin/$(TARGET)-freetype-config --libs`" \
+		-DFREETYPE_INCLUDE_DIR_ft2build="$(PREFIX)/$(TARGET)/include/freetype2" \
+		-DFREETYPE_INCLUDE_DIR_freetype2="$(PREFIX)/$(TARGET)/include" \
+		-DFREETYPE_INCLUDE_DIRS="$(PREFIX)/$(TARGET)/include/freetype2;$(PREFIX)/$(TARGET)/include"
 		-DVTK_MODULE_ENABLE_VTK_libproj=NO \
 		-DVTK_MODULE_USE_EXTERNAL_VTK_freetype=ON \
 		-DVTK_REQUIRE_LARGE_FILE_SUPPORT_EXITCODE=0 \

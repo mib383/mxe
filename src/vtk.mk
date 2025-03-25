986adbf26320aa2c3e77953802344349fa414162
mib383 <mib383@gmail.com>
2025-03-25 23:52:13 +0700

    one more fix

--- src/vtk.mk
+++ src/vtk.mk
@@ -45,7 +45,7 @@ define $(PKG)_BUILD
 	
 	
 	echo "`$(PREFIX)/$(TARGET)/bin/freetype-config`"
-	FTLIBS=$($(PREFIX)/$(TARGET)/bin/freetype-config --libs)
+	FTLIBS="$(eval "$($(PREFIX)/$(TARGET)/bin/freetype-config --libs")"
 	FTDIR1=$(PREFIX)/$(TARGET)/include/freetype2
 	FTDIR2=$(PREFIX)/$(TARGET)/include/
 	FTDIRS=$(FTDIR1);$(FTDIR1)
@@ -68,7 +68,7 @@ define $(PKG)_BUILD
         -DVTK_FORBID_DOWNLOADS=ON \
         -DBUILD_EXAMPLES=OFF \
         -DBUILD_TESTING=OFF \
-		-DFREETYPE_LIBRARIES="`$(PREFIX)/$(TARGET)/bin/freetype-config --libs`" \
+		-DFREETYPE_LIBRARIES="$(eval "$($(PREFIX)/$(TARGET)/bin/freetype-config --libs")" \
 		-DFREETYPE_INCLUDE_DIR_ft2build="$(PREFIX)/$(TARGET)/include/freetype2" \
 		-DFREETYPE_INCLUDE_DIR_freetype2="$(PREFIX)/$(TARGET)/include" \
 		-DFREETYPE_INCLUDE_DIRS="$(PREFIX)/$(TARGET)/include/freetype2;$(PREFIX)/$(TARGET)/include" \

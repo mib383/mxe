ce65c6cb53dcfb01cf011205952d09d02ff51f08
mib383 <mib383@gmail.com>
2025-03-26 01:07:09 +0700

    one more fix

--- src/vtk.mk
+++ src/vtk.mk
@@ -70,12 +70,12 @@ define $(PKG)_BUILD
 		-DH5_LONG_TO_LDOUBLE_SPECIAL_RUN=0 -DH5_LONG_TO_LDOUBLE_SPECIAL_RUN__TRYRUN_OUTPUT="" \
 		-DH5_DISABLE_SOME_LDOUBLE_CONV_RUN=0 -DH5_DISABLE_SOME_LDOUBLE_CONV_RUN__TRYRUN_OUTPUT="" \
         $(PKG_CONFIGURE_OPTS)
-	echo 'target_link_libraries(VTK::RenderingFreeType INTERFACE $(shell $(PREFIX)/$(TARGET)/bin/freetype-config --libs))' >> $(BUILD_DIR)/lib/cmake/vtk-$(PKG)_VERSION/vtk-config.cmake
+	echo 'target_link_libraries(VTK::RenderingFreeType INTERFACE $(shell $(PREFIX)/$(TARGET)/bin/freetype-config --libs))' >> $(BUILD_DIR)/lib/cmake/vtk-9.4/vtk-config.cmake
     $(MAKE) -C '$(BUILD_DIR)' -j '$(JOBS)' VERBOSE=1
     $(MAKE) -C '$(BUILD_DIR)' -j 1 install VERBOSE=1
 	
 	#echo 'target_link_libraries(Freetype::Freetype INTERFACE $(PREFIX)/$(TARGET)/lib/libbrotlidec.a $(PREFIX)/$(TARGET)/lib/libbrotlienc.a $(PREFIX)/$(TARGET)/lib/libbrotlicommon.a $(PREFIX)/$(TARGET)/lib/libbz2.a)' >> $(PREFIX)/$(TARGET)/lib/cmake/vtk-$(call SHORT_PKG_VERSION,$(PKG))/vtk-config.cmake
-	echo 'target_link_libraries(VTK::RenderingFreeType INTERFACE $(shell $(PREFIX)/$(TARGET)/bin/freetype-config --libs))' >> $(PREFIX)/$(TARGET)/lib/cmake/vtk-$(PKG)_VERSION/vtk-config.cmake
+	echo 'target_link_libraries(VTK::RenderingFreeType INTERFACE $(shell $(PREFIX)/$(TARGET)/bin/freetype-config --libs))' >> $(PREFIX)/$(TARGET)/lib/cmake/vtk-9.4/vtk-config.cmake
     #now build the GUI -> Qt -> SimpleView Example
     mkdir '$(BUILD_DIR).test'
 	# echo 'target_link_libraries(SimpleView PRIVATE $(PREFIX)/$(TARGET)/lib/libbrotlidec.a)' >> $(SOURCE_DIR)/Examples/GUI/Qt/SimpleView/CMakeLists.txt
